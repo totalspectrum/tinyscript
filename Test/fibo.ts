cadcf576b6f4bd2f9e793d959e7cbc14aec064ec
Eric Smith <ersmith@totalspectrum.ca>
2016-04-10 20:17:34 -0300

    compressed script a bit

--- Test/fibo.ts
+++ Test/fibo.ts
@@ -5,21 +5,16 @@
 # calculate fibo(n)
 # parameter in n
 # returns result in r
-
 var n=0
 var r=0
-
 proc fibo {
   if (n<2) {
     r=n
   } else {
     var saven=n  # save our parameter for recursive call
-    n=n-1
-    fibo
-    var a=r
-    n=saven-2
-    fibo
-    r=r+a
+    var a=0
+    n=n-1; fibo; a=r
+    n=saven-2; fibo; r=a+r
     n=saven
   }
 }
